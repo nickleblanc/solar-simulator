a3d1e86e0ee6b984bd389bf10f1b3542840568f2
nickleblanc <nick.leblanc@unb.ca>
2024-03-11 12:55:05 -0300

    Removed sidecar and added api fetch

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -3,11 +3,27 @@
 import { useEffect, useState } from "react";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { useLocationStore } from "@/stores/data";
+import { useQuery } from "@tanstack/react-query";
 import { Graph } from "@/components/Graph";
 
 export function Dashboard() {
   const locations = useLocationStore((state) => state.locations);
   const selectedLocations = locations.filter((location) => location.selected);
+
+  const fetchData = async () => {
+    const response = await fetch("http://localhost:5001/hist");
+    return response.json();
+  };
+
+  const data = useQuery({ queryKey: ["data"], queryFn: fetchData });
+
+  if (data.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (data.isError) {
+    return <div>Error: {data.error.message}</div>;
+  }
 
   const totalArea = selectedLocations.reduce(
     (acc, location) => acc + location.area,
@@ -15,42 +31,27 @@ export function Dashboard() {
   );
 
   return (
-    <div className="flex flex-col justify-between mt-2 mr-2">
-      <div className="flex flex-row">
-        <Card className="grow">
-          <CardHeader>
-            <CardTitle>Total Production</CardTitle>
-          </CardHeader>
-          <CardContent>{}</CardContent>
-        </Card>
-        <Card className="grow">
-          <CardHeader>
-            <CardTitle>Total Cost</CardTitle>
-          </CardHeader>
-          <CardContent>{}</CardContent>
-        </Card>
-        <Card className="grow">
-          <CardHeader>
-            <CardTitle>Total Surface Area</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <span>{totalArea} m²</span>
-          </CardContent>
-        </Card>
-        {locations.map((location) => (
-          <Card key={location.id} className="grow">
-            <CardHeader>
-              <CardTitle>{location.name}</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <span>
-                {location.selected == true ? "Selected" : "Not Selected"}
-              </span>
-            </CardContent>
-          </Card>
-        ))}
-      </div>
-      <Graph />
+    <div className="flex justify-between mt-2 mr-2">
+      <Card className="grow">
+        <CardHeader>
+          <CardTitle>Total Production</CardTitle>
+        </CardHeader>
+        <CardContent>{}</CardContent>
+      </Card>
+      <Card className="grow">
+        <CardHeader>
+          <CardTitle>Total Cost</CardTitle>
+        </CardHeader>
+        <CardContent>{}</CardContent>
+      </Card>
+      <Card className="grow">
+        <CardHeader>
+          <CardTitle>Total Surface Area</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <span>{totalArea} m²</span>
+        </CardContent>
+      </Card>
     </div>
   );
 }
--- lib/test.ts
+++
@@ -1,9 +0,0 @@
-import { Command } from "@tauri-apps/api/shell";
-
-export async function TestPython() {
-  // const command = Command.sidecar("py/test", ["value", "test"]);
-  const command = Command.sidecar("py/test");
-  const output = await command.execute();
-  const { stdout, stderr } = output;
-  console.log(stdout);
-}
