ce7eaec8c8e7ffa7b4a6f05c7ce06a8e144e51d4
Nick <nick.leblanc@unb.ca>
2024-03-11 12:08:51 -0300

    Initial commit for solar simulation api

---
+++ src/histsearch.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 29 10:37:30 2024
+
+@author: noahj
+"""
+
+def histsearch(lat,lon,start,end,panelnum,panelseries,panel_data,inverter_data):
+    import pvlib
+    from pvlib import pvsystem 
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import pandas as pd
+    import openmeteo_requests
+    from datetime import datetime ,timedelta,date
+    import requests_cache
+    import pandas as pd
+    from retry_requests import retry
+    # from requests_cache import DO_NOT_CACHE 
+    
+    ## Calling weather API openmeteo
+    # Setup the Open-Meteo API client with cache and retry on error
+    cache_session = requests_cache.CachedSession('.cache',cache_control=True, expire_after=120)
+    retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+    openmeteo = openmeteo_requests.Client(session = retry_session)
+    
+    cutoff=date.today()-timedelta(weeks=12)
+    
+    if start>=cutoff :
+        url = "https://api.open-meteo.com/v1/forecast"
+        params = {
+	"latitude": lat,
+	"longitude": lon,
+	"minutely_15": ["temperature_2m", "direct_normal_irradiance"],
+	"timezone": "auto",
+	"start_date": start,
+	"end_date": end
+}
+    
+        responses = openmeteo.weather_api(url, params=params)
+
+        # Process first location. Add a for-loop for multiple locations or weather models
+        response = responses[0]
+
+        # Process minutely_15 data. The order of variables needs to be the same as requested.
+        minutely_15 = response.Minutely15()
+        minutely_15_temperature_2m = minutely_15.Variables(0).ValuesAsNumpy()
+        minutely_15_direct_normal_irradiance = minutely_15.Variables(1).ValuesAsNumpy()
+    
+        minutely_15_data = {"date": pd.date_range(
+            start = pd.to_datetime(minutely_15.Time(), unit = "s", utc = True),
+            end= pd.to_datetime(minutely_15.TimeEnd(), unit = "s", utc = True),
+            freq = pd.Timedelta(seconds = minutely_15.Interval()),
+            inclusive = "left"
+             )}
+        minutely_15_data["temperature_2m"] = minutely_15_temperature_2m
+        minutely_15_data["direct_normal_irradiance"] = minutely_15_direct_normal_irradiance
+
+        minutely_15_dataframe = pd.DataFrame(data = minutely_15_data)
+
+    ## Running Solar model
+
+        cec_fit_params = pvlib.ivtools.sdm.fit_cec_sam('monoSi', panel_data['V_mp_ref'], panel_data['I_mp_ref'],
+                         panel_data['V_oc_ref'], panel_data['I_sc_ref'], panel_data['alpha_sc'],
+                        panel_data['beta_oc'], panel_data['gamma_r'], 
+                        panel_data['N_s'], 25)
+
+        irrad=minutely_15_direct_normal_irradiance
+        temp_amb=minutely_15_temperature_2m
+        temp_cell=temp_amb+(41-20)/800*irrad
+        
+        
+        # 2nd step: Apply model to estimate the 5 parameters of the single diode equation using the CEC model
+
+
+
+        diode_params = pvlib.pvsystem.calcparams_cec(irrad, temp_cell, panel_data['alpha_sc'], cec_fit_params[4], 
+                                                    cec_fit_params[0], cec_fit_params[1], cec_fit_params[3], 
+                                                    cec_fit_params[2], cec_fit_params[5])
+        # This returns the parameters needed for model
+        
+        iv_values1 = pvlib.pvsystem.singlediode(diode_params[0], 
+                                                diode_params[1], 
+                                                diode_params[2], 
+                                                diode_params[3], 
+                                                diode_params[4], 
+                                                ivcurve_pnts=25,   # Number of points of the I-V curve (equally distributed)
+                                                method='lambertw') # I-V using the Lambert W. function
+        acpower =pvlib.inverter.sandia(iv_values1['v_mp']*panelseries, # DC voltage input to the inverter
+                                        iv_values1['p_mp']*panelnum, # DC power input to the inverter
+                                         inverter_data) # Parameters for the inverter 
+        times=minutely_15_data['date']-timedelta(hours=4)
+       ## Plotting
+        times=times.strftime("%m/%d %H:%M")
+        i=0
+        xpos=[0]*int((len(times)/48))
+        while i<(len(times)/48):
+            xpos[i]=i*48
+            i=i+1
+        xlabs=times[xpos]
+        plt.figure(1)
+        plt.plot(times,acpower)
+        plt.title("AC power Generation 3 Day Forecast")
+        plt.xlabel("Date and Time")
+        plt.ylabel("AC Power Generated (W)")
+        plt.xticks(xpos,xlabs,rotation='vertical')
+        return times, acpower
+    else:
+      url = "https://archive-api.open-meteo.com/v1/archive"
+      params = {
+	"latitude": lat,
+	"longitude": lon,
+	"start_date": start,
+	"end_date": end,
+	"hourly": ["temperature_2m", "direct_normal_irradiance"],
+	"timezone": "auto"
+        }
+      responses = openmeteo.weather_api(url, params=params)
+
+    # Process first location. Add a for-loop for multiple locations or weather models
+    response = responses[0]  
+    
+    
+    # Process hourly data. The order of variables needs to be the same as requested.
+    hourly = response.Hourly()
+    hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+    hourly_direct_normal_irradiance = hourly.Variables(1).ValuesAsNumpy()
+
+    hourly_data = {"date": pd.date_range(
+	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = hourly.Interval()),
+	inclusive = "left"
+)}
+    hourly_data["temperature_2m"] = hourly_temperature_2m
+    hourly_data["direct_normal_irradiance"] = hourly_direct_normal_irradiance
+
+    hourly_dataframe = pd.DataFrame(data = hourly_data)
+    ## Running Solar model
+
+    cec_fit_params = pvlib.ivtools.sdm.fit_cec_sam('monoSi', panel_data['V_mp_ref'], panel_data['I_mp_ref'],
+                         panel_data['V_oc_ref'], panel_data['I_sc_ref'], panel_data['alpha_sc'],
+                        panel_data['beta_oc'], panel_data['gamma_r'], 
+                        panel_data['N_s'], 25)
+
+    irrad=hourly_direct_normal_irradiance
+    temp_amb=hourly_temperature_2m
+    temp_cell=temp_amb+(41-20)/800*irrad
+        
+        
+        # 2nd step: Apply model to estimate the 5 parameters of the single diode equation using the CEC model
+
+
+
+    diode_params = pvlib.pvsystem.calcparams_cec(irrad, temp_cell, panel_data['alpha_sc'], cec_fit_params[4], 
+                                                    cec_fit_params[0], cec_fit_params[1], cec_fit_params[3], 
+                                                    cec_fit_params[2], cec_fit_params[5])
+        # This returns the parameters needed for model
+        
+    iv_values1 = pvlib.pvsystem.singlediode(diode_params[0], 
+                                                diode_params[1], 
+                                                diode_params[2], 
+                                                diode_params[3], 
+                                                diode_params[4], 
+                                                ivcurve_pnts=25,   # Number of points of the I-V curve (equally distributed)
+                                                method='lambertw') # I-V using the Lambert W. function
+    acpower =pvlib.inverter.sandia(iv_values1['v_mp']*panelseries, # DC voltage input to the inverter
+                                        iv_values1['p_mp']*panelnum, # DC power input to the inverter
+                                         inverter_data) # Parameters for the inverter 
+    times=hourly_data['date']-timedelta(hours=4)
+       ## Plotting
+    times=times.strftime("%m/%d %H:%M")
+    i=0
+    xpos=[0]*int((len(times)/12))
+    while i<(len(times)/12):
+            xpos[i]=i*12
+            i=i+1
+    xlabs=times[xpos]
+    # plt.figure(2)
+    # plt.plot(times,acpower)
+    # plt.title("AC power Generation 3 Day Forecast")
+    # plt.xlabel("Date and Time")
+    # plt.ylabel("AC Power Generated (W)")
+    # plt.xticks(xpos,xlabs,rotation='vertical')
+    # print("Test")
+    # print(acpower)
+    return acpower
---
+++ src/server.py
@@ -0,0 +1,68 @@
+from flask import Flask
+from flask import jsonify
+from flask_cors import CORS
+from histsearch import histsearch
+from datetime import datetime ,timedelta,date
+
+server = Flask(__name__)
+CORS(server)
+        
+lat="45.964993";
+lon="-66.646332";
+stc=600;
+ptc=454;
+v_mp=44;
+i_mp=13.64
+v_oc=51.8
+i_sc=14.54
+alpha=0.05
+beta=-0.25
+gamma=-0.29
+n_s=144
+pac=3800
+pdc=6000
+vdc=600
+panelnum=10
+panelsrs=2
+
+
+## entering panel paramters
+module_data = {'Technology': 'monocrystalline Q.ANTUM solar half cells', # technology
+         'STC': stc, # STC power
+         'PTC': ptc, # PTC power
+         'V_mp_ref': v_mp, # Maximum power voltage under STC
+         'I_mp_ref': i_mp, # Maximum power current under STC
+         'V_oc_ref': v_oc, # Open-circuit voltage under STC
+         'I_sc_ref': i_sc, # Short-circuit current under STC
+         'alpha_sc': alpha, # Temperature Coeff. Short Circuit Current [A/C]
+         'beta_oc': beta, # Temperature Coeff. Open Circuit Voltage [V/C]
+         'gamma_r': gamma, # Temperature coefficient of power at maximum point [%/C]
+         'N_s': n_s, # Number of cells in series
+         'temp_ref': 25}  # Reference temperature conditions
+
+
+inverter = {'Paco': pac,   # AC Power rating  User provided
+                     'Pso': pac*0.01, # power consumed by inverter 1% of Ac rating is reasonable 
+                     'Pdco': pdc, # Dc power input resulting in Paco at reference voltage   User provided
+                     'Vdco': vdc, # dc reference voltage     User provided
+                    'C0': 0, #  (assumed 0 without experimental data)
+                    'C1': 0, #  (assumed 0 without experimental data)
+                    'C2': 0, # (assumed 0 without experimental data)
+                    'C3': 0, # (assumed 0 without experimental data)
+                    'Pnt': 1, # Power consumed at night
+                   } 
+
+start = '2023,1,8'
+end = '2023,1,10'
+
+start=start.split(",",-1)
+end=end.split(",",-1)
+start=date(int(start[0]),int(start[1]),int(start[2]))
+end=date(int(end[0]),int(end[1]),int(end[2]))
+
+@server.route('/hist')
+def hist():
+    return jsonify({'histsearch': histsearch(lat=lat, lon=lon, start=start, end=end, panelnum=panelnum, panelseries=panelsrs, panel_data=module_data, inverter_data=inverter).tolist()})
+
+if __name__ == '__main__':
+    server.run(host='0.0.0.0', port=5001)
