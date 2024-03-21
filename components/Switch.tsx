3b15d6f3cdd475c27b761463703385f99b1e5c4c
Nick <nick.leblanc@unb.ca>
2024-03-20 23:35:35 -0300

    Removed console.log

--- components/Switch.tsx
+++ components/Switch.tsx
@@ -12,11 +12,9 @@ export function SwitchSlider({ id }: SwitchSliderProps) {
   const setSelected = useLocationStore((state) => state.setSelected);
 
   const [isChecked, setChecked] = useState(true);
-  console.log(isChecked);
 
   useEffect(() => {
     setSelected(id, isChecked);
-    // console.log(locations);
   }, [setSelected, isChecked, id]);
 
   return (
