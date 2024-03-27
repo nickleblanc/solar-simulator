3511b3ae066848094f47444ea6cfada7d3583edb
Nick <nick.leblanc@unb.ca>
2024-03-27 12:35:20 -0300

    Fixed setValue of form-values to allow null

--- stores/form-values.ts
+++ stores/form-values.ts
@@ -5,7 +5,7 @@ import { create } from "zustand";
 interface FormState {
   lat: number | null;
   lng: number | null;
-  setValues: (lat: number, lng: number) => void;
+  setValues: (lat: number | null, lng: number | null) => void;
 }
 
 export const useFormValueStore = create<FormState>((set) => ({
